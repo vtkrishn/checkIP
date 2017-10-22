17bd07466a2c581aa748d855fdb8ed2390aabe31
Vinod Krishnan <vin.sinin@gmail.com>
2017-10-22 01:02:36 -0700

    changes

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var helper = require('../index');
+describe('#lcheckIP.check', function() {
+          it('return value', function() {
+              var result = 1;
+              expect(result.val).to.equal(1);
+          });
+});
